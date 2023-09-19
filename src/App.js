9f70ea9c884e861b78a667e6fb76b32c332cc39c
aryan-prog <mandelakalam12364@gmail.com>
2023-09-19 14:59:12 +0530

    api change

--- src/App.js
+++ src/App.js
@@ -22,11 +22,12 @@ function App() {
 
   const options = {
     method: "GET",
-    url: "https://open-weather13.p.rapidapi.com/city/" + city,
-    headers: {
-      "X-RapidAPI-Key": "d4cd6fdee9msh45c03025bfbd61bp1b25e9jsn5cf65e0a2276",
-      "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
-    },
+    url: "https://weatherapi-com.p.rapidapi.com/current.json",
+    params: {q: city},
+  headers: {
+    'X-RapidAPI-Key': 'd4cd6fdee9msh45c03025bfbd61bp1b25e9jsn5cf65e0a2276',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  },
   };
 
   const [data, setData] = useState({
@@ -35,12 +36,6 @@ function App() {
     wind: { speed: 0 },
     weather: [{ main: "" }],
   });
-
-  const fahrenheitToCelsius = (fahrenheit) => {
-    // Convert Fahrenheit to Celsius using the formula: (Fahrenheit - 32) * 5/9
-    const celsius = (fahrenheit - 32) * (5 / 9);
-    return celsius;
-  };
 
   const searchWeather = async () => {
     try {
@@ -52,67 +47,30 @@ function App() {
     }
   };
 
-  const currentTimestamp = Date.now();
-  let sunriseTimestamp = 0;
-  let sunsetTimestamp = 0;
-  if (data.sys && data.sys.sunrise) {
-    sunriseTimestamp = data.sys.sunrise;
-  } else {
-    console.error("Sunrise data not found in JSON.");
-  }
-  if (data.sys && data.sys.sunset) {
-    sunsetTimestamp = data.sys.sunset;
-  } else {
-    console.error("Sunset data not found in JSON.");
-  }
-  console.log(sunriseTimestamp);
-  console.log(sunsetTimestamp);
-
-  const isDaytime = () => {
-    // Convert timestamps to milliseconds
-    const currentMillis = currentTimestamp * 1000;
-    const sunriseMillis = sunriseTimestamp * 1000;
-    const sunsetMillis = sunsetTimestamp * 1000;
-
-    // Create Date objects for the current time, sunrise, and sunset
-    const currentDate = new Date(currentMillis);
-    const sunriseDate = new Date(sunriseMillis);
-    const sunsetDate = new Date(sunsetMillis);
-
-    // Compare the current time with sunrise and sunset
-    if (currentDate >= sunriseDate && currentDate <= sunsetDate) {
-      console.log("day");
-      return true; // It's daytime
-    } else {
-      console.log("night")
-      return false; // It's nighttime
-    }
-  };
   let weatherImage = clear;
-  // console.log(data.weather[0]?.description);
-  // console.log(isDaytime);
 
-  if (isDaytime) {
-    if (data.weather[0].description === "haze") weatherImage = mist;
-    else if (data.weather[0]?.description?.includes("clouds"))
+  if (data?.current?.is_day) {
+    if (data?.current?.condition?.text === "mist") weatherImage = mist;
+    else if (data?.current?.condition?.text.includes("clouds"))
       weatherImage = cloudsDay;
-    else if (data.weather[0]?.description?.includes("rain"))
+    else if (data?.current?.condition?.text.includes("rain"))
       weatherImage = rain;
-    else if (data.weather[0]?.description?.includes("drizzle"))
+    else if (data?.current?.condition?.text.includes("drizzle"))
       weatherImage = drizzle;
   } else {
     weatherImage = clearMoon;
-    if (data.weather[0].description === "haze") weatherImage = starMoon;
-    else if (data.weather[0]?.description?.includes("clouds"))
+    if (data?.current?.condition?.text === "mist") weatherImage = starMoon;
+    else if (data?.current?.condition?.text.includes("clouds"))
       weatherImage = cloudsNight;
-    else if (data.weather[0]?.description?.includes("rain"))
+    else if (data?.current?.condition?.text.includes("rain"))
       weatherImage = rain;
-    else if (data.weather[0]?.description?.includes("drizzle"))
+    else if (data?.current?.condition?.text.includes("drizzle"))
       weatherImage = drizzle;
   }
 
-  if (data.weather[0]?.description?.includes("snow")) weatherImage = snow;
-  else if (data.weather[0]?.description?.includes("lightning"))
+  if (data?.current?.condition?.text.includes("snow")) 
+  weatherImage = snow;
+  else if (data?.current?.condition?.text.includes("lightning"))
     weatherImage = bijli;
 
   return (
@@ -134,28 +92,28 @@ function App() {
             ></img>
           </button>
         </div>
-        {data.name && (
+        {data.location?.name && (
           <div className="weather">
             <div className="main">
               <img src={weatherImage} alt="" className="weather-icon" />
               <h1 className="temp">
-                {Math.round(fahrenheitToCelsius(data.main.temp))}°c
+                {data?.current?.temp_c}°c
               </h1>
-              <h2 className="city">{data.name}</h2>
+              <h2 className="city">{data?.location?.name}</h2>
             </div>
 
             <div className="details">
               <div className="col">
                 <img src={humidity} alt="" />
               </div>
-              <p className="humidity">{data.main.humidity} %</p>
+              <p className="humidity">{data?.current?.humidity} %</p>
               <p>Humidity</p>
             </div>
             <div className="details">
               <div className="col">
                 <img src={wind} alt="" />
               </div>
-              <p className="wind">{data.wind.speed} km/h</p>
+              <p className="wind">{data?.current?.wind_kph} km/h</p>
               <p>Wind Speed</p>
             </div>
           </div>
